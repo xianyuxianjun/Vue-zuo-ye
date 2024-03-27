5fed17d165512086737c53ef96687ceb74a8195e
咸鱼仙君 <1796792003@qq.com>
2024-03-27 20:40:41 +0800

    第五次课后作业

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="app">
+<!--    <One></One>-->
+<!--    <Tow></Tow>-->
+<!--    <Three></Three>-->
+<!--    <Four></Four>-->
+<!--    <Five></Five>-->
+<!--    <P66 v-if="zu==1"></P66>-->
+    <P67 v-if="zu==1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"></P67>
+    <button @click="xiaohui">点我销毁组件,再次点击生成组件</button>
+
+  </div>
+</template>
+
+<script setup>
+  import One from "@/day01/One.vue";
+  import Tow from "@/day01/Three.vue";
+  import Four from "@/day01/Four.vue";
+  import Three from "@/day01/Three.vue";
+  import Five from "@/day01/Five.vue";
+  import P66 from "@/day02/P66.vue";
+  import {ref} from "vue";
+  import P67 from "@/day02/P67.vue";
+  let zu=ref(1)
+  function xiaohui(){
+    if (zu.value==1){
+      zu.value=0
+    }else{
+      zu.value=1
+    }
+  }
+</script>
+
+<style scoped>
+.app {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ src/day01/One.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="one">
+    <h2>sum的值为：{{sum}}</h2>
+    <button @click="SunAdd">sum+1</button>
+  </div>
+</template>
+
+<script setup name="One">
+  import {ref,watch} from "vue";
+  let sum = ref(0);
+  function SunAdd(){
+    sum.value = sum.value + 1
+  }
+  const stop = watch(sum, (newValue, oldValue) => {
+    console.log(`sum的值发生了变化，旧的值为 ${oldValue}, 新的值为 ${newValue}`);
+    if (newValue >= 10) {
+      stop(); // 停止侦听
+    }
+  });
+
+</script>
+
+<style scoped>
+
+</style>
